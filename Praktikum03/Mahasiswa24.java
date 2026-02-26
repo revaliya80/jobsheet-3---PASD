66218f238676793d54d346567a9702c7de6a2006
Refaliya-Vinandha <revaliya80@gmail.com>
2026-02-27 00:32:44 +0700

    Percobaan dan modifikasi

---
+++ Praktikum03/Mahasiswa24.java
@@ -0,0 +1,16 @@
+package Praktikum03;
+
+public class Mahasiswa24 {
+    public String nim;
+    public String nama;
+    public String kelas;
+    public float ipk;
+
+    public void cetakInfo() {
+        System.out.println("NIM : " + nim);
+        System.out.println("Nama : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK : " + ipk);
+        System.out.println("-----------------------------");
+    }
+}
